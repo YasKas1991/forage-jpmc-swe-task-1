c85b4624f2ae761f8b718080c5c528ae817b1203
Yaser Kasim <YaserKasim@outlook.com>
2023-12-19 20:26:05 +0000

    Updated client_test.py unit tests

--- client_test.py
+++ client_test.py
@@ -7,17 +7,45 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+  
+  
+     # Arrange[Expected results]
+    expected_results = [
+    ('ABC', 120.48, 121.2, (120.48 + 121.2)/2),
+    ('DEF', 117.87, 121.68, (117.87 + 121.68)/2)
+]
+
+     
+     
+     # Act: Call function and store output
+    for i in range (len(quotes)):
+       result = getDataPoint(quotes[i])
+       print(f"Test {i}: Expected {expected_results[i]}, Got {result}") # Test for mismatch, identify and clearly define issue
+    # Assert: Verify that function returns expected results
+       self.assertEqual(result, expected_results[i])
+
+
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
 
 
-  """ ------------ Add more unit tests ------------ """
+
+    # Arrange[Expected results]
+    expected_results = [
+    ('ABC', 120.48, 119.2, (120.48 + 119.2)/2),
+    ('DEF', 117.87, 121.68, (117.87 + 121.68)/2)
+]
+
+
+    # Act and Assert
+    for i in range (len(quotes)):
+       result = getDataPoint(quotes[i])
+       print(f"Test {i}: Expected {expected_results[i]}, Got {result}")
+       self.assertEqual(result, expected_results[i])
 
 
 
